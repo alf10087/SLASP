9bd5407e674fe3327862763add35f584c39ce756
alf10087 <alf10087@gmail.com>
2022-03-30 13:41:27 -0500

    Not done yet, something came up

--- HPS to PAGL.R
+++ HPS to PAGL.R
@@ -7,15 +7,24 @@ hps <- read.csv("/Users/alfonso/Downloads/HPS_Week29_PUF_CSV/pulse2021_puf_29.cs
 ### Keep the variables:
 
 hps_c <- hps %>% select(TBIRTH_YEAR, EGENDER, RHISPANIC, RRACE, 
-                        EEDUC, MS, THHLD_NUMPER, THHLD_NUMKID, THHLD_NUMADLT, 
-                        HADCOVID, WRKLOSSRV, EXPCTLOSS, ANYWORK, 
-                        KINDWORK, TW_YN, TW_COV, WKVOL, SETTING, ANXIOUS,
-                        WORRY, INTEREST, DOWN, PRESCRIPT, MH_SVCS, MH_NOTGET, 
-                        TENURE, LIVQTRRV, EVICT, TNUM_PS, PSCHNG1, PSCHNG2,
-                        PSCHNG3, PSCHNG4, PSCHNG5, PSCHNG6, 
+                        EEDUC, THHLD_NUMPER, 
+                        HADCOVID, ANYWORK, EVICT,
+                        ANXIOUS, WORRY, INTEREST, DOWN, PRESCRIPT, MH_SVCS, MH_NOTGET, 
                         PSWHYCHG1, PSWHYCHG2,
                         PSWHYCHG3, PSWHYCHG4, PSWHYCHG5, PSWHYCHG6, 
                         PSWHYCHG7, PSWHYCHG8, PSWHYCHG9, EST_ST, EST_MSA, 
                         REGION)
 
 hps_c <- hps_c %>% mutate(age = 2021 - TBIRTH_YEAR)
+
+hps_c <- hps_c %>%
+  rename(hispanic = RHISPANIC, race = RRACE, alone = THHLD_NUMPER, 
+         female = EGENDER, educ = EEDUC, had_covid = HADCOVID, 
+         anxious = ANXIOUS, prescription = PRESCRIPT, region = REGION, 
+         employed = ANYWORK) %>%
+  select(-TBIRTH_YEAR) %>%
+  mutate(female = female - 1) %>%
+  mutate(hispanic = hispanic - 1) %>%
+  mutate(alone = ifelse(alone == 1, 1, 0)) %>%
+  mutate(had_covid = ifelse(had_covid == 2, 0, had_covid)) %>%
+  mutate(had_covid = ifelse(had_covid == 3, 0, had_covid))
