0d4c8b345d1791d3829d6b192794260d3028a9e7
Melanie Milligan <melanie.milligan@utexas.edu>
2022-04-05 19:38:16 -0500

    Add write.csv line to end of file that combines HPS and PAGL data

--- final_merge.R
+++ final_merge.R
@@ -3,22 +3,25 @@
 rm(list=ls())
 library(dplyr)
 
+# Load Files
 hps <- read.csv("C:/Users/Melanie/Documents/UT Austin/PAGL/SLAS/SLASP/hps_clean.csv")
 pagl <- read.csv("C:/Users/Melanie/Documents/UT Austin/PAGL/SLAS/SLASP/pagl.csv")
 
-# Remove X variable from CSV
+# Remove X primary key from HPS and PAGL
 hps <- hps %>%
   select(-X)
 
 pagl <- pagl %>%
   select(-X)
 
+# Merge PAGL and HPS
 full <- merge(hps, pagl, all = TRUE)
 
-
+# Look at the data
 z <- ncol(full)
 for (i in 1:z) {
   print(colnames(full)[i])
   print(table(full[,i]))
 }
 
+write.csv(full, "full.csv")
